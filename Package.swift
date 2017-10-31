15b3e7dbd969a0f9f64a90be38e0477e2f7a83d8
krad <iam@krad.io>
2017-10-31 10:36:13 -0700

    Switching up the library type in the Package manifest

--- Package.swift
+++ Package.swift
@@ -6,21 +6,11 @@ import PackageDescription
 let package = Package(
     name: "Buffie",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Buffie",
-            targets: ["Buffie"]),
+        .library(name: "Buffie", type: .dynamic, targets: ["Buffie"]),
     ],
-    dependencies: [
-    ],
+    dependencies: [ ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Buffie",
-            dependencies: []),
-        .testTarget(
-            name: "BuffieTests",
-            dependencies: ["Buffie"]),
+        .target(name: "Buffie", dependencies: []),
+        .testTarget(name: "BuffieTests", dependencies: ["Buffie"]),
     ]
 )
