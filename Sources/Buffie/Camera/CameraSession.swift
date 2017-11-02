29fa0a8281ed4e3a8dbe4745b180d5936c2ebc43
krad <iam@krad.io>
2017-11-01 17:28:52 -0700

    Adding ability to enumerate over available audio / video devices

--- Sources/Buffie/Camera/CameraSession.swift
+++ Sources/Buffie/Camera/CameraSession.swift
@@ -98,7 +98,32 @@ internal class CameraSession: CameraSessionProtocol {
 }
 
 
-extension AVCaptureDevice {
+public extension AVCaptureDevice {
+    
+    public static func videoDevices() -> [AVCaptureDevice] {
+        return AVCaptureDevice.devices(for: .video)
+    }
+    
+    public static func videoDevicesDictionary() -> [String: String] {
+        var result: [String: String] = [:]
+        for device in self.videoDevices() {
+            result[device.uniqueID] = device.localizedName
+        }
+        return result
+    }
+    
+    public static func audioDevices() -> [AVCaptureDevice] {
+        return AVCaptureDevice.devices(for: .audio)
+    }
+    
+    public static func audioDevicesDictionary() -> [String: String] {
+        var result: [String: String] = [:]
+        for device in self.audioDevices() {
+            result[device.uniqueID] = device.localizedName
+        }
+        return result
+    }
+
     
     static func firstDevice(for mediaType: AVMediaType, in position: AVCaptureDevice.Position) throws -> AVCaptureDevice {
         let devices = AVCaptureDevice.devices(for: mediaType).filter { $0.position != position }
---
+++ Tests/BuffieTests/DeviceEnumerationTests.swift
@@ -0,0 +1,42 @@
+import XCTest
+import AVFoundation
+@testable import Buffie
+
+class DeviceEnumerationTests: XCTestCase {
+
+    func test_that_we_can_get_a_list_of_available_video_devices() {
+        XCTAssertNotNil(AVCaptureDevice.videoDevices())
+        XCTAssert(AVCaptureDevice.videoDevices().count > 0 )
+     
+        XCTAssertNotNil(AVCaptureDevice.videoDevicesDictionary())
+        XCTAssert(AVCaptureDevice.videoDevicesDictionary().keys.count > 0)
+        
+        let deviceID = AVCaptureDevice.videoDevicesDictionary().keys.first
+        XCTAssertNotNil(deviceID)
+        
+        let device = AVCaptureDevice(uniqueID: deviceID!)
+        XCTAssertNotNil(device)
+    }
+    
+    func test_that_we_can_get_a_list_of_available_audio_devices() {
+        XCTAssertNotNil(AVCaptureDevice.audioDevices())
+        XCTAssert(AVCaptureDevice.audioDevices().count > 0 )
+        
+        XCTAssertNotNil(AVCaptureDevice.audioDevicesDictionary())
+        XCTAssert(AVCaptureDevice.audioDevicesDictionary().keys.count > 0)
+
+        let deviceID = AVCaptureDevice.audioDevicesDictionary().keys.first
+        XCTAssertNotNil(deviceID)
+
+        let device = AVCaptureDevice(uniqueID: deviceID!)
+        XCTAssertNotNil(device)
+
+    }
+    
+    static var allTests = [
+        ("test_that_we_can_get_a_list_of_available_video_devices", test_that_we_can_get_a_list_of_available_video_devices),
+        ("test_that_we_can_get_a_list_of_available_audio_devices", test_that_we_can_get_a_list_of_available_audio_devices),
+        ]
+
+    
+}
