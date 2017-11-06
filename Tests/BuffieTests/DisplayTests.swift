500258e5e45c7927c115ff401d797551a11b3644
krad <iam@krad.io>
2017-11-05 21:57:57 -0800

    Added ability to get the localized display name from the IORegistry

--- Tests/BuffieTests/DisplayTests.swift
+++ Tests/BuffieTests/DisplayTests.swift
@@ -18,12 +18,26 @@ class DisplayTests: XCTestCase {
         XCTAssert(result.count > 0)
     }
     
+    func test_that_we_can_get_a_screen_input() {
+        let result = Display.getAll().first
+        XCTAssertNotNil(result)
+        XCTAssertNotNil(result?.input)
+    }
+    
+    func test_that_we_can_get_a_localized_name_for_the_display() {
+        let result = Display.getAll().first
+        XCTAssertEqual("Color LCD", result?.name)
+    }
+    
     static var allTests = [
         ("test_that_we_can_get_a_list_of_display_ids",
          test_that_we_can_get_a_list_of_display_ids),
 
         ("test_that_we_can_get_a_list_of_displays",
-         test_that_we_can_get_a_list_of_displays)
+         test_that_we_can_get_a_list_of_displays),
+        
+        ("test_that_we_can_get_a_screen_input",
+         test_that_we_can_get_a_screen_input),
     ]
     
 }
