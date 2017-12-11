b0e416078562ba6e5911b7919157cd64874d870a
krad <iam@krad.io>
2017-12-11 16:33:10 -0700

    Switching up timestamps.  May have to disregard the timing info completely for calculations

--- Sources/Buffie/Capture/AVReader.swift
+++ Sources/Buffie/Capture/AVReader.swift
@@ -71,7 +71,7 @@ internal class VideoSampleReader: NSObject, AVCaptureVideoDataOutputSampleBuffer
             let prevPTS  = CMSampleBufferGetPresentationTimeStamp(prevSampleBuffer)
             let currPTS  = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
             let duration =  CMTimeSubtract(currPTS, prevPTS)
-            if let newSample = self.createNewSample(from: prevSampleBuffer, with: duration, and: prevPTS) {
+            if let newSample = self.createNewSample(from: prevSampleBuffer, with: duration, and: currPTS) {
                 self.delegate?.got(newSample, type: .video)
                 self.samples.removeLast()
                 self.samples.append(sampleBuffer)
