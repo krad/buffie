5caf93dd5a096476fec2c9c3f33b66c679768069
krad <iam@krad.io>
2017-12-10 17:33:23 -0700

    Removing garbage from the AAC encoder.  Verified it works on iOS.  Broken on macOS

--- Sources/Buffie/Audio/AACEncoder.swift
+++ Sources/Buffie/Audio/AACEncoder.swift
@@ -11,7 +11,7 @@ public class AACEncoder {
     private var audioConverter: AudioConverterRef?
     fileprivate var aacBuffer: [UInt8] = []
     fileprivate var aacBufferSize: UInt32
-    private var pcmBuffer: [UInt8] = []
+    private var pcmBuffer: [UInt8]     = []
     private var pcmBufferSize: UInt32
     
     fileprivate var fillComplexCallback: AudioConverterComplexInputDataProc = { (inAudioConverter, 
@@ -45,23 +45,8 @@ public class AACEncoder {
                 outASBD.mBitsPerChannel     = 0
                 outASBD.mReserved           = 0
                 
-//                #if os(macOS)
-                    let status = AudioConverterNew(&inASBD, &outASBD, &audioConverter)
-                    if status != noErr { print("Failed to setup converter:", status) }
-//                #else
-//                if var descriptor = self.getAudioClassDescription(with: kAudioFormatMPEG4AAC,
-//                                                                  from: kAppleSoftwareAudioCodecManufacturer) {
-//
-//                    let status = AudioConverterNewSpecific(&inASBD,
-//                                                           &outASBD,
-//                                                           1,
-//                                                           &descriptor,
-//                                                           &audioConverter)
-//                    if status != noErr { print("Failed to setup converter:", status) }
-//
-//                }
-//                #endif
-
+                let status = AudioConverterNew(&inASBD, &outASBD, &audioConverter)
+                if status != noErr { print("Failed to setup converter:", status) }
             }
         }
     }
@@ -83,11 +68,6 @@ public class AACEncoder {
             outBuffer[0].mDataByteSize = self.pcmBufferSize
             outBuffer[0].mData = UnsafeMutableRawPointer.allocate(bytes: Int(self.pcmBufferSize), alignedTo: 0)
             
-//            var outBuffer                      = AudioBufferList()
-//            outBuffer.mNumberBuffers           = 1
-//            outBuffer.mBuffers.mNumberChannels = 1
-//            outBuffer.mBuffers.mDataByteSize   = 1024
-        
             var ioOutputDataPacketSize: UInt32 = 1
         
             let status = AudioConverterFillComplexBuffer(audioConverter,
@@ -99,6 +79,7 @@ public class AACEncoder {
         
             if status == noErr {
                 let rawAAC = Data(self.aacBuffer)
+                print(self.aacBuffer)
                 onComplete(rawAAC, noErr)
             } else {
                 print("Error converting buffer:", status)
@@ -113,64 +94,19 @@ public class AACEncoder {
         ioData: UnsafeMutablePointer<AudioBufferList>,
         outDataPacketDescription: UnsafeMutablePointer<UnsafeMutablePointer<AudioStreamPacketDescription>?>?) -> OSStatus
     {
-        print(#function)
         let requestedPackets = ioNumberDataPackets.pointee
         self.pcmBuffer.withUnsafeMutableBufferPointer { bufferPtr in
             let ptr                              = UnsafeMutableRawPointer(bufferPtr.baseAddress)
             ioData.pointee.mBuffers.mData        = ptr
             ioData.pointee.mBuffers.mDataByteSize = self.pcmBufferSize
-            print(ioData)
         }
         
         if self.pcmBufferSize < requestedPackets {
-            print("Fuck")
             ioNumberDataPackets.pointee = 0
             return -1
         }
         
-        print("Next")
         ioNumberDataPackets.pointee = 1
         return noErr
     }
-    
-
-    // http://stackoverflow.com/questions/10817036/can-i-use-avcapturesession-to-encode-an-aac-stream-to-memory
-    private func getAudioClassDescription(with type: UInt32,
-                                          from manufacturer: UInt32) -> AudioClassDescription?
-    {
-        var status = noErr
-        
-        var encoderSpec: UInt32 = type
-        var size: UInt32 = 0
-        status = AudioFormatGetPropertyInfo(kAudioFormatProperty_Encoders,
-                                            type,
-                                            &encoderSpec,
-                                            &size)
-        
-        if status != noErr {
-            print("Error getting audio format property", status)
-            return nil
-        }
-        
-        var descriptions: [AudioClassDescription] = []
-        status = AudioFormatGetProperty(kAudioFormatProperty_Encoders,
-                                        type,
-                                        &encoderSpec,
-                                        &size,
-                                        &descriptions)
-        
-        if status != noErr {
-            print("Error getting audio format property", status)
-            return nil
-        }
-        
-        for descriptor in descriptions {
-            if descriptor.mSubType == type && descriptor.mManufacturer == manufacturer {
-                return descriptor
-            }
-        }
-        
-        return nil
-    }
-    
 }
